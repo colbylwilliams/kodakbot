fe82a4e44fcfc3848e15159892146455a4031a47
Colby Williams <colbylwilliams@gmail.com>
2017-11-17 17:01:11 -0500

    form tweak

--- Models/PhotoOrder.cs
+++ Models/PhotoOrder.cs
@@ -4,21 +4,31 @@ using Microsoft.Bot.Builder.FormFlow;
 
 namespace KodakBot
 {
-    public enum OrderTypeOptions { SinglePhoto, MultiplePhotos }
-    public enum SizeOptions { FourBySix, FiveBySeven, SixByNine }
+
+    public enum OrderOptions { PrintASinglePhoto, PrintMultiplePhotos }
+    public enum SizeOptions { FourXSix, FiveXSeven, SixXNine }
     public enum PickupStoreOptions { CVS, RiteAid, Walgreens, Target }
 
     [Serializable]
     public class PhotoOrder
     {
-        public OrderTypeOptions? OrderType;
+
+        [Prompt("\nWhat can I help you with? {||}")]
+        public OrderOptions? OrderType;
+
+        [Prompt("\nWhat? {||}")]
         public SizeOptions? Size;
+
+        [Prompt("\nGreat, what's your {&}?")]
+        public int Zipcode;
+
+        [Prompt("\nWe have several stores in your area, where would you like to pick up your prints? {||}")]
         public PickupStoreOptions? Store;
 
         public static IForm<PhotoOrder> BuildForm()
         {
             return new FormBuilder<PhotoOrder>()
-                    .Message("Welcome to the Kodak bot!")
+                    //.Message("Welcome to the Kodak bot!")
                     .Build();
         }
     }
